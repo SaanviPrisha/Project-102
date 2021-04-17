88151294276d3c4a0d42908d1daee868b4978126
SaanviPrisha <69468899+SaanviPrisha@users.noreply.github.com>
2021-04-16 21:40:44 -0400

    Update Study Planner.py

--- Study Planner.py
+++ Study Planner.py
@@ -1,16 +1,17 @@
-print("  DAILY STUDY TIME")
-totalDaysLeft = input("Days left to study (num): ")
+totalDayLeft = input("Days left to study: ")
+totalDaysLeft = int(totalDayLeft)
 chapters = input("Number of chapters total: ")
 averageTimePerChapter = input("The average time you take per chapter in hours: ")
 
 totalTimeToBeSpent = float(chapters)*float(averageTimePerChapter)
-print("You need to study for ",totalTimeToBeSpent,"hours in all. ")
-print("Which means,")
 
 timePerDay = float(totalTimeToBeSpent)/float(totalDaysLeft)
 print("You need to spend ",timePerDay," hours studying everyday")
-print("or you can spend",timePerDay*2,"hours studying alternate days")
-print("In minutes: ")
-print("You need to spend ",timePerDay*60," minutes studying everyday")
-print("or you can spend",timePerDay*120,"minutes studying alternate days, to keep the flow")
-58
+
+if(timePerDay >= 10):
+    print("You needed to start studying earlier! Remeber that for the future... but for now start cramming. ")
+elif(totalDaysLeft >= 15 and timePerDay <= 6):
+    print("You have a whole bunch of time, but there isn't anytime to study then like the future.")
+else:
+    print("The ideal time to start studying. Start making your planner!")
+
